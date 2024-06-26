6132ca42a8116e556a0c6ea96f254c7aa801fa97
MazuttiGuilherme <mazuttimail@gmail.com>
2024-06-26 16:53:14 -0300

    Criando Classes Cliente e Endereco em model

---
+++ src/main/java/com/estudos/projeto_dio_springboot/model/Cliente.java
@@ -0,0 +1,41 @@
+package com.estudos.projeto_dio_springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(startegy = GenerationType.Auto)
+    private long id;
+    private String nome;
+    @ManyToOne
+    private Endereco endereco;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
