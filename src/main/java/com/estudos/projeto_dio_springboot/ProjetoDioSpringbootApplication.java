71f82e5dd890d58642c0eabf7723fd3cbfa95cae
MazuttiGuilherme <mazuttimail@gmail.com>
2024-06-26 15:47:26 -0300

    Criando um projeto no Spring Initializr

---
+++ src/main/java/com/estudos/projeto_dio_springboot/ProjetoDioSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.projeto_dio_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDioSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDioSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/projeto_dio_springboot/ProjetoDioSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.projeto_dio_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDioSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
